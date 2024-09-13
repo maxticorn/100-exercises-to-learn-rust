66ab9e6c440e6a497a5216d9722aec322718a43f
maxticorn <maksticorn@gmail.com>
2024-09-13 23:15:49 +0100

    basic_calculator-while

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result = 1;
+    let mut n_copy = n;
+    while n_copy > 0 {
+        result *= n_copy;
+        n_copy -= 1;
+    }
+    result
 }
 
 #[cfg(test)]
